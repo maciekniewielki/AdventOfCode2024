f23e7205c4d8d16a608f4c8f568e6d8719873935
maciekniewielki <kadlubken5@gmail.com>
2025-02-16 22:11:27 +0100

    Updated day 9 with namedtuple

--- Python/day09/day09.py
+++ Python/day09/day09.py
@@ -1,5 +1,8 @@
 from Common import *
 from itertools import zip_longest
+from collections import namedtuple
+
+Chunk = namedtuple("Chunk", ["id", "size"])
 
 
 def compact_blocks(blocks):
@@ -18,7 +21,7 @@ def compact_blocks(blocks):
 
 
 def compact_chunks(chunks):
-    highest_id = max(filter(lambda c: c[0] is not None, chunks), key=lambda c: c[0])[0]
+    highest_id = max(filter(lambda c: c.id is not None, chunks), key=lambda c: c.id).id
     for id in range(highest_id, 0, -1):
         try_insert_chunk(chunks, id)
     return chunks
@@ -27,21 +30,23 @@ def compact_chunks(chunks):
 def try_insert_chunk(chunks, chunk_id):
     upper = None
     for i in range(len(chunks) - 1, 0, -1):
-        if chunks[i][0] == chunk_id:
+        if chunks[i].id == chunk_id:
             upper = i
 
     for lower in range(upper):
-        if chunks[lower][0] is None and chunks[lower][1] == chunks[upper][1]:
+        if chunks[lower].id is None and chunks[lower].size == chunks[upper].size:
             # File is just the right size, just swap places
             chunks[lower], chunks[upper] = chunks[upper], chunks[lower]
             break
-        elif chunks[lower][0] is None and chunks[lower][1] > chunks[upper][1]:
+        elif chunks[lower].id is None and chunks[lower].size > chunks[upper].size:
             # File is smaller than free space
-            free_space_left = chunks[lower][1] - chunks[upper][1]
+            free_space_left = chunks[lower].size - chunks[upper].size
             # Insert whole file into the free space and free space into file
-            chunks[lower], chunks[upper] = chunks[upper], (None, chunks[upper][1])
+            chunks[lower], chunks[upper] = chunks[upper], Chunk(
+                None, chunks[upper].size
+            )
             # Add leftover free space after the file
-            chunks.insert(lower + 1, (None, free_space_left))
+            chunks.insert(lower + 1, Chunk(None, free_space_left))
             break
 
 
@@ -56,7 +61,7 @@ def calc_checksum(blocks):
 def blockify(chunks):
     blocks = []
     for chunk in chunks:
-        blocks += [chunk[0]] * chunk[1]
+        blocks += [chunk.id] * chunk.size
     return blocks
 
 
@@ -74,18 +79,15 @@ def solve2(chunks):
 
 # IO
 a = input_as_string("input.txt")
-
-
 chunks = []
 for file_id, space in enumerate(zip_longest(a[::2], a[1::2])):
     file_space, free_space = space
-    chunks.append((file_id, int(file_space)))
+    chunks.append(Chunk(file_id, int(file_space)))
     if free_space:
-        chunks.append((None, int(free_space)))
+        chunks.append(Chunk(None, int(free_space)))
 
 # 1st
 print(solve1(chunks))
-
 
 # 2nd
 print(solve2(chunks))
